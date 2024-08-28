fe18c2ed4266fb13b8c25c0fdefd5be152a871f4
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2024-08-28 09:43:02 -0400

    refactor: cleanup

--- R/app_server.R
+++
@@ -1,5 +0,0 @@
-app_server <- function(input, output, session) {
-
-  mod_header_server("header")
-  mod_sidebar_server(id = "sidebar")
-}
--- R/gmhleasr-package.R
+++ R/gmhleasr-package.R
@@ -2,9 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom glue glue
 #' @importFrom rlang .data
 #' @importFrom rlang .env
 #' @importFrom tibble tibble
+#' @importFrom glue glue
+#' @importFrom htmltools tags
 ## usethis namespace: end
 NULL
