9146ec035f3166c682987a9dccd0fdb15979a158
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2024-08-29 13:30:39 -0400

    tests: cleanup

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -7,6 +7,42 @@
 #  ------------------------------------------------------------------------
 
 
+# mocks -------------------------------------------------------------------
+
+mock_req_body <- list(
+  auth = list(
+    type = "basic"
+  ),
+  requestId = 15,
+  method = list(
+    name = "getStatus",
+    version = "r1",
+    params = list()
+  )
+)
+
+mock_res_success <- list(
+  response = list(
+    requestId = "15",
+    code = 200,
+    result = list(
+      status = "Success",
+      message = "API service is available and running."
+    )
+  )
+)
+
+mock_res_error <- list(
+  response = list(
+    requestId = "15",
+    error = list(
+      code = 113,
+      message = "Username and/or password is incorrect."
+    )
+  )
+)
+
+
 # mock response -----------------------------------------------------------
 
 # Helper function to create a mock response
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -11,9 +11,27 @@ options(
   warn = 1
 )
 
-cfg <- config::get("entrata", file = here::here("config.yml"))
+Sys.setenv("R_CONFIG_FILE" = system.file("config/config.yml", package = "gmhleasr"))
+
+cfg <- config::get("entrata")
+
 cfg_test <- list(
   username = "testuser",
   password = "testpass",
   base_url = "https://api.entrata.com"
 )
+
+test_prop_ids <- c(
+  "739084",
+  "641240",
+  "676055",
+  "952515",
+  "518041",
+  "518042",
+  "833617",
+  "1197887",
+  "1143679",
+  "1311849"
+)
+
+
