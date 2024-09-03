2745046de1fe18481a7fafe4ee654ed02f78bc9e
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2024-09-03 18:10:32 -0400

    feat: R utility functions

--- R/utils.R
+++ R/utils.R
@@ -23,6 +23,27 @@ view_list <- function(
   listviewer::jsonedit(listdata, ...)
 }
 
+#' @keywords internal
+#' @noRd
 is_testing <- function() {
   identical(Sys.getenv("TESTTHAT"), "true")
+}
+
+#' Is Boolean String
+#'
+#' @description
+#' Checks if provided string represents a boolean (used by Entrata API).
+#'
+#' @param str Character string to check. Typically, with the Entrata API, "boolean"
+#'   values are represented as quoted integers (`"0"` or `"1"`) representing
+#'   `FALSE` and `TRUE`, respectively.
+#'
+#' @return `TRUE` if the string is a boolean string, `FALSE` otherwise.
+#'
+#' @export
+#'
+#' @examples
+#' is_boolean_string("0")
+is_boolean_string <- function(str) {
+  str %in% c(as.character(as.integer(TRUE)), as.character(as.integer(FALSE)))
 }
---
+++ R/utils_shiny.R
@@ -0,0 +1,87 @@
+
+#  ------------------------------------------------------------------------
+#
+# Title : Shiny Utilities
+#    By : Jimmy Briggs
+#  Date : 2024-08-30
+#
+#  ------------------------------------------------------------------------
+
+
+
+#' Fluid Column - Shiny fluidRow + Column
+#'
+#' @param ... elements to include within the flucol
+#' @param width width
+#' @param offset offset
+#'
+#' @return A column wrapped in fluidRow
+#' @export
+#'
+#' @importFrom shiny fluidRow column
+flucol <- function(..., width = 12, offset = 0) {
+
+  if (!is.numeric(width) || (width < 1) || (width > 12))
+    stop("column width must be between 1 and 12")
+
+  shiny::fluidRow(
+    shiny::column(
+      width = width,
+      offset = offset,
+      ...
+    )
+  )
+}
+
+#' Insert Logo
+#'
+#' @param file file
+#' @param style style
+#' @param width width
+#' @param ref ref
+#'
+#' @return tag
+#' @export
+#' @importFrom shiny tags img
+insert_logo <- function(
+    file,
+    style = "background-color: #FFF; width: 100%; height: 100%;",
+    width = NULL,
+    ref = "#"
+) {
+
+  shiny::tags$div(
+    style = style,
+    shiny::tags$a(
+      shiny::img(
+        src = file,
+        width = width
+      ),
+      href = ref
+    )
+  )
+
+}
+
+#' Icon Text
+#'
+#' Creates an HTML div containing the icon and text.
+#'
+#' @param icon fontawesome icon
+#' @param text text
+#'
+#' @return HTML div
+#' @export
+#'
+#' @examples
+#' icon_text("table", "Table")
+#'
+#' @importFrom htmltools tagList
+#' @importFrom shiny icon
+icon_text <- function(icon, text) {
+
+  if (is.character(icon)) i <- shiny::icon(icon) else i <- icon
+  t <- paste0(" ", text)
+  htmltools::tagList(htmltools::tags$div(i, t))
+
+}
